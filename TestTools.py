e7163f01356b080f4aca3c0523fb7b4088fa4bc3
Andrei Demian <ano.andrei.demian@gmail.com>
2019-05-04 12:08:15 +0300

    Return the last day of the week relative to current date

---
+++ TestTools.py
@@ -0,0 +1,11 @@
+from last_day_of_week import last_day_from_week
+import StringCompress
+
+##### The date of last day of week ( specific date ) #####
+#date = last_day_from_week("Sunday", year=2018,month=7,day=1)
+
+##### The date of last day of week ( current day ) #####
+date = last_day_from_week("MONDAY")
+
+## Print Date
+print(date)
