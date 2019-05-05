949dfeb91a599e206a136ddb349d60a3a9a6144c
Andrei Demian <ano.andrei.demian@gmail.com>
2019-05-05 16:46:28 +0300

    Tools

--- TestTools.py
+++ TestTools.py
@@ -1,11 +1,5 @@
-from last_day_of_week import last_day_from_week
-import StringCompress
+from StringCompress import compress
 
-##### The date of last day of week ( specific date ) #####
-#date = last_day_from_week("Sunday", year=2018,month=7,day=1)
+example_string = "JsjjjkkkkkkaAAAAAkskvvvvvSSSSSSVVV"
 
-##### The date of last day of week ( current day ) #####
-date = last_day_from_week("MONDAY")
-
-## Print Date
-print(date)
+print(compress(example_string))
